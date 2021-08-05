949b27aa3b9386ed656c4a2b1dd07bb1e6fd173f
Svetoslav Petrov <s.petrov.bg@gmail.com>
2021-08-06 00:42:51 +0300

    fixing small bug in visualization when team emblem is not loading correctly and finish logic for calculate and populate points for prediction

--- Services/SportBattles.Services.Data/PredictionsService.cs
+++ Services/SportBattles.Services.Data/PredictionsService.cs
@@ -52,47 +52,74 @@
             foreach (var prediction in predictions)
             {
                 var doublePoints = this.matchesService.IsDoublePoint(prediction.GameId, prediction.MatchId);
-                var points = this.CalculatePoints(prediction.HomeGoals, prediction.AwayGoals, prediction.Match.HomeGoals, prediction.Match.AwayGoals, prediction.Game.GameType, doublePoints);
+                var points = this.CalculatePoints(prediction);
                 prediction.Points = (byte?)points;
             }
 
             await this.predictionRepository.SaveChangesAsync();
         }
 
-        private int? CalculatePoints(byte? predictionHomeGoals, byte? predictionAwayGoals, byte? homeGoals, byte? awayGoals, GameType gameType, bool doublePoints)
+        private int? CalculatePoints(Prediction prediction)
         {
-            if (predictionHomeGoals is null || predictionAwayGoals is null || homeGoals is null || awayGoals is null)
+            if (prediction.HomeGoals is null || prediction.AwayGoals is null || prediction.Match.HomeGoals is null || prediction.Match.AwayGoals is null)
             {
                 return null;
             }
 
-            var multiplier = doublePoints ? 2 : 1;
-
-            if (predictionHomeGoals == homeGoals && predictionAwayGoals == awayGoals)
+            var multiplier = this.matchesService.IsDoublePoint(prediction.GameId, prediction.MatchId) ? 2 : 1;
+            var maxResult = 0;
+            foreach (var gamePoint in prediction.Game.GameType.GamePoints)
             {
-                return gameType.GamePoints.FirstOrDefault(p => p.Name == "ExactScorelinePoints").Value * multiplier;
+                var result = gamePoint.Name switch
+                {
+                    "Exact scoreline" => this.GetPoints(prediction, gamePoint, multiplier, this.ExactScorelinePoints()),
+                    "Goal difference" => this.GetPoints(prediction, gamePoint, multiplier, this.GoalDifferencePoints()),
+                    "Goals scored by one of the teams" => this.GetPoints(prediction, gamePoint, multiplier, this.OneTeamGoalsPoints()),
+                    "Outcome" => this.GetPoints(prediction, gamePoint, multiplier, this.OutcomePoints()),
+                    _ => throw new ArgumentException("Add method to calculate points for this GamePointType first!"),
+                };
+
+                if (result > maxResult)
+                {
+                    maxResult = result;
+                }
             }
 
-            if (predictionHomeGoals - predictionAwayGoals == homeGoals - awayGoals)
-            {
-                return gameType.GamePoints.FirstOrDefault(p => p.Name == "GoalDifferencePoints").Value * multiplier;
-            }
+            return maxResult;
+        }
 
-            var predictionOutcome = predictionHomeGoals - predictionAwayGoals;
-            var resultOutcome = homeGoals - awayGoals;
-            var outcome = (predictionOutcome == 0 && resultOutcome == 0) || (predictionOutcome * resultOutcome > 0);
 
-            if (outcome && (predictionHomeGoals == homeGoals || predictionAwayGoals == awayGoals))
-            {
-                return gameType.GamePoints.FirstOrDefault(p => p.Name == "OneTeamGoalsPoints").Value * multiplier;
-            }
 
-            if (outcome)
+        private int GetPoints(Prediction prediction, GamePoint gamePoint, int multiplier, Func<Prediction, bool> condition)
+        {
+            if (condition(prediction))
             {
-                return gameType.GamePoints.FirstOrDefault(p => p.Name == "OutcomePoints").Value * multiplier;
+                return gamePoint.Value * multiplier;
             }
 
             return 0;
+        }
+
+        private Func<Prediction, bool> ExactScorelinePoints() => (prediction) =>
+            prediction.HomeGoals == prediction.Match.HomeGoals &&
+            prediction.AwayGoals == prediction.Match.AwayGoals;
+
+        private Func<Prediction, bool> GoalDifferencePoints() => (prediction) =>
+            prediction.HomeGoals - prediction.AwayGoals == prediction.Match.HomeGoals - prediction.Match.AwayGoals;
+
+        private Func<Prediction, bool> OneTeamGoalsPoints() => (prediction) =>
+            (this.IsCorrectWinner(prediction) &&
+            (prediction.HomeGoals == prediction.Match.HomeGoals || prediction.AwayGoals == prediction.Match.AwayGoals));
+
+        private Func<Prediction, bool> OutcomePoints() => (prediction) =>
+            this.IsCorrectWinner(prediction);
+
+        private bool IsCorrectWinner(Prediction prediction)
+        {
+            var predictionOutcome = prediction.HomeGoals - prediction.AwayGoals;
+            var resultOutcome = prediction.Match.HomeGoals - prediction.Match.AwayGoals;
+            var outcome = (predictionOutcome == 0 && resultOutcome == 0) || (predictionOutcome * resultOutcome > 0);
+            return outcome;
         }
     }
 }
